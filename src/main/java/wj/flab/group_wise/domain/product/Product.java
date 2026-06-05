a970e8b6eba37c6dc5c1d1ec757b7962f77eae4a
woojung.roh <woojung8405@gmail.com>
2026-06-05 16:52:54 +0900

    fix(product): @version import 문 수정

--- src/main/java/wj/flab/group_wise/domain/product/Product.java
+++ src/main/java/wj/flab/group_wise/domain/product/Product.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Version;
 import jakarta.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
